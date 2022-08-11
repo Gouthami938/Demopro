2c3fba051483c5a30efc76eac86c9979fe284a8e
gowthamipurr22@gmail.comgit config --global user.name gowthamipurr22@gmail.comgit config --global user.name gowthamipurr22@gmail.com <gowthamipurr22@gmail.com>
2022-08-11 07:46:59 +0530

    updated Program of Calculating Marks

--- src/MarksProgram.java
+++ src/MarksProgram.java
@@ -23,15 +23,15 @@ public class MarksProgram {
             System.out.println("Enter roll");
             rollno[i] = sc.nextInt();
             //  name[i] = sc.nextLine();
-            // english[i] = sc.nextInt();
-            //  computerscience[i] = sc.nextInt();
-            // social[i] = sc.nextInt();
+             english[i] = sc.nextInt();
+              computerscience[i] = sc.nextInt();
+             social[i] = sc.nextInt();
             maths[i] = sc.nextInt();
             science[i] = sc.nextInt();
             totalmarks[i] = (english[i] + computerscience[i] + social[i] + maths[i] + science[i]);
             avgMarks[i] = (english[i] + computerscience[i] + social[i] + maths[i] + science[i]) / 5;
-            System.out.println("Total marks of student" + i + " is " + totalmarks[i]);
-            System.out.println("average marks of student" + i + " is " + avgMarks[i]);
+            System.out.println("Total marks of student" + rollno[i] + " is " + totalmarks[i]);
+            System.out.println("average marks of student" + rollno[i] + " is " + avgMarks[i]);
             //  System.out.println(Arrays.sort(totalmarks[i]));
         }
         int totalmark[] = totalmarks;
@@ -50,22 +50,40 @@ public class MarksProgram {
         System.out.println("Top one Performer marks"+totalmark[total]);
         System.out.println("Top two Performer marks"+totalmark[total-1]);
         System.out.println("Top three Performer marks"+totalmark[total-2]);
-//Average Performers
-        for( i=0;i<students;i++){
+//Below Average Performers
+        for(  i=0;i<students;i++){
             if(totalmarks[i]<50){
-                System.out.println(" student "+rollno[i]+" total marks "+totalmarks[i]);
+                System.out.println(" AveragePerformer with student rollno:"+rollno[i]+" total marks "+totalmarks[i]);
+            }
+        }
+//average perfomer in each subject
+        for(  i=0;i<students;i++){
+            if(maths[i]<50){
+                System.out.println(" AveragePerformer of student"+rollno[i]+" total marks "+totalmarks[i]+" in maths");
+            }
+        }
+        for(  i=0;i<students;i++){
+            if(english[i]<50){
+                System.out.println(" AveragePerformer of student"+rollno[i]+" total marks "+totalmarks[i]+" in english");
+            }
+        }
+        for(  i=0;i<students;i++){
+            if(computerscience[i]<50){
+                System.out.println(" AveragePerformer of student"+rollno[i]+" total marks "+totalmarks[i]+" in computerscience");
+            }
+        }
+        for(  i=0;i<students;i++){
+            if(social[i]<50){
+                System.out.println(" AveragePerformer of student"+rollno[i]+" total marks "+totalmarks[i]+" in social");
+            }
+        }
+        for(  i=0;i<students;i++){
+            if(science[i]<50){
+                System.out.println(" AveragePerformer of student"+rollno[i]+" total marks "+totalmarks[i]+" in science");
             }
         }
 
-        int rank = 1;
-        for (int j = 0; j < totalmarks.length; j++) {
-            //  for (int k = 0; k < students; k++) {
-            if (totalmarks[j] > totalmarks[total]) {
-                total = j;
-            }
 
-        }
-        System.out.println(rollno[total] + " got " + totalmarks[total] + " so " + rank);
 
     }
 
